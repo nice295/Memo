c75da164068b40447aee98112f7f1c416375c91e
nice295 <nice295@gmail.com>
2016-09-28 23:00:28 +0900

    add recorder

---
+++ Test/AndroidAudioRecorder-master/app/src/main/java/cafe/adriel/androidaudiorecorder/AndroidAudioRecorder.java
@@ -0,0 +1,77 @@
+package cafe.adriel.androidaudiorecorder;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.graphics.Color;
+import android.os.Environment;
+
+import cafe.adriel.androidaudiorecorder.model.AudioChannel;
+import cafe.adriel.androidaudiorecorder.model.AudioSampleRate;
+import cafe.adriel.androidaudiorecorder.model.AudioSource;
+
+public class AndroidAudioRecorder {
+
+    protected static final String EXTRA_FILE_PATH = "filePath";
+    protected static final String EXTRA_COLOR = "color";
+    protected static final String EXTRA_SOURCE = "source";
+    protected static final String EXTRA_CHANNEL = "channel";
+    protected static final String EXTRA_SAMPLE_RATE = "sampleRate";
+
+    private Activity activity;
+
+    private String filePath = Environment.getExternalStorageDirectory() + "/recorded_audio.wav";
+    private AudioSource source = AudioSource.MIC;
+    private AudioChannel channel = AudioChannel.STEREO;
+    private AudioSampleRate sampleRate = AudioSampleRate.HZ_44100;
+    private int color = Color.parseColor("#546E7A");
+    private int requestCode = 0;
+
+    private AndroidAudioRecorder(Activity activity) {
+        this.activity = activity;
+    }
+
+    public static AndroidAudioRecorder with(Activity activity) {
+        return new AndroidAudioRecorder(activity);
+    }
+
+    public AndroidAudioRecorder setFilePath(String filePath) {
+        this.filePath = filePath;
+        return this;
+    }
+
+    public AndroidAudioRecorder setColor(int color) {
+        this.color = color;
+        return this;
+    }
+
+    public AndroidAudioRecorder setRequestCode(int requestCode) {
+        this.requestCode = requestCode;
+        return this;
+    }
+
+    public AndroidAudioRecorder setSource(AudioSource source) {
+        this.source = source;
+        return this;
+    }
+
+    public AndroidAudioRecorder setChannel(AudioChannel channel) {
+        this.channel = channel;
+        return this;
+    }
+
+    public AndroidAudioRecorder setSampleRate(AudioSampleRate sampleRate) {
+        this.sampleRate = sampleRate;
+        return this;
+    }
+
+    public void record() {
+        Intent intent = new Intent(activity, AudioRecorderActivity.class);
+        intent.putExtra(EXTRA_FILE_PATH, filePath);
+        intent.putExtra(EXTRA_COLOR, color);
+        intent.putExtra(EXTRA_SOURCE, source);
+        intent.putExtra(EXTRA_CHANNEL, channel);
+        intent.putExtra(EXTRA_SAMPLE_RATE, sampleRate);
+        activity.startActivityForResult(intent, requestCode);
+    }
+
+}
---
+++ Test/AndroidAudioRecorder-master/app/src/main/java/cafe/adriel/androidaudiorecorder/Util.java
@@ -0,0 +1,81 @@
+package cafe.adriel.androidaudiorecorder;
+
+import android.app.Activity;
+import android.content.pm.PackageManager;
+import android.graphics.Color;
+import android.media.AudioFormat;
+import android.os.Handler;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+
+import cafe.adriel.androidaudiorecorder.model.AudioChannel;
+import cafe.adriel.androidaudiorecorder.model.AudioSampleRate;
+import cafe.adriel.androidaudiorecorder.model.AudioSource;
+
+
+public class Util {
+    private static final Handler HANDLER = new Handler();
+
+    private Util() {
+    }
+
+    public static void requestPermission(Activity activity, String permission) {
+        if (ContextCompat.checkSelfPermission(activity, permission)
+                != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(activity, new String[]{permission}, 0);
+        }
+    }
+
+    public static void wait(int millis, Runnable callback){
+        HANDLER.postDelayed(callback, millis);
+    }
+
+    public static omrecorder.AudioSource getMic(AudioSource source,
+                                                AudioChannel channel,
+                                                AudioSampleRate sampleRate) {
+        return new omrecorder.AudioSource.Smart(
+                source.getSource(),
+                AudioFormat.ENCODING_PCM_16BIT,
+                channel.getChannel(),
+                sampleRate.getSampleRate());
+    }
+
+    public static boolean isBrightColor(int color) {
+        if(android.R.color.transparent == color) {
+            return true;
+        }
+        int [] rgb = {Color.red(color), Color.green(color), Color.blue(color)};
+        int brightness = (int) Math.sqrt(
+                rgb[0] * rgb[0] * 0.241 +
+                rgb[1] * rgb[1] * 0.691 +
+                rgb[2] * rgb[2] * 0.068);
+        return brightness >= 200;
+    }
+
+    public static int getDarkerColor(int color) {
+        float factor = 0.8f;
+        int a = Color.alpha(color);
+        int r = Color.red(color);
+        int g = Color.green(color);
+        int b = Color.blue(color);
+        return Color.argb(a,
+                Math.max((int) (r * factor), 0),
+                Math.max((int) (g * factor), 0),
+                Math.max((int) (b * factor), 0));
+    }
+
+    public static String formatSeconds(int seconds) {
+        return getTwoDecimalsValue(seconds / 3600) + ":"
+                + getTwoDecimalsValue(seconds / 60) + ":"
+                + getTwoDecimalsValue(seconds % 60);
+    }
+
+    private static String getTwoDecimalsValue(int value) {
+        if (value >= 0 && value <= 9) {
+            return "0" + value;
+        } else {
+            return value + "";
+        }
+    }
+
+}
